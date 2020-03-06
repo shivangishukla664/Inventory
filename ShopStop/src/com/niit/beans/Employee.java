d868834cfb9d6168ddacc985c50228144168f20d
Shivangi <shivangishukla272@gmail.com>
2020-03-06 16:01:18 +0530

    My Rest Service

---
+++ ShopStop/src/com/niit/beans/Employee.java
@@ -0,0 +1,30 @@
+package com.niit.beans;
+
+public class Employee {
+	private int empno;
+	private String empname;
+	private String desig;
+	public Employee() {
+	
+		// TODO Auto-generated constructor stub
+	}
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getDesig() {
+		return desig;
+	}
+	public void setDesig(String desig) {
+		this.desig = desig;
+	}
+	
+}
